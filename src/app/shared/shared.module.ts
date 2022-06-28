2ab3bba836d627f4d88ae6bc840552f2510c9164
Mark Dolormente <markjosephandinodolormente@gmail.com>
2022-06-28 17:48:42 +0800

    Create UI for home/user page

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { HeaderComponent } from './header/header.component';
@@ -10,6 +12,6 @@ import { HeaderComponent } from './header/header.component';
 @NgModule({
   declarations: [HeaderComponent],
   imports: [CommonModule, MatButtonModule, MatIconModule, MatToolbarModule],
-  exports: [HeaderComponent, MatButtonModule],
+  exports: [HeaderComponent, MatButtonModule, MatFormFieldModule, MatIconModule, MatInputModule],
 })
 export class SharedModule {}
