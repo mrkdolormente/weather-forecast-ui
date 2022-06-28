9f8aea749dc1b8fd73261dc128b1a38a4d80fa41
Mark Dolormente <markjosephandinodolormente@gmail.com>
2022-06-29 02:52:57 +0800

    Integrate user info endpoint

--- src/app/pages/home/home-weather/home-weather.component.ts
+++ src/app/pages/home/home-weather/home-weather.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subject, takeUntil } from 'rxjs';
+import { UsersService } from 'src/app/core/services/users.service';
+import { UserInfo } from 'src/app/models/users.interface';
 
 @Component({
   selector: 'app-home-weather',
   templateUrl: './home-weather.component.html',
-  styleUrls: ['./home-weather.component.scss']
+  styleUrls: ['./home-weather.component.scss'],
 })
-export class HomeWeatherComponent implements OnInit {
+export class HomeWeatherComponent implements OnInit, OnDestroy {
+  destroy$ = new Subject();
+  userInfo?: UserInfo;
 
-  constructor() { }
+  constructor(private readonly userService: UsersService) {}
 
   ngOnInit(): void {
+    this.userService
+      .getUserInfo()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: (userInfo: UserInfo) => {
+          this.userInfo = userInfo;
+        },
+      });
   }
 
+  ngOnDestroy(): void {
+    this.destroy$.next(true);
+    this.destroy$.unsubscribe();
+  }
 }
